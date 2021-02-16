487f518f2359df5172f6664bbcf2360ba7d20767
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2021-02-16 09:05:51 +0100

    Ejercicio persistencia

---
+++ ejercicio-jpa-kotlin/src/main/kotlin/com/salesianostriana/dam/ejerciciojpakotlin/Entidades.kt
@@ -0,0 +1,138 @@
+package com.salesianostriana.dam.ejerciciojpakotlin
+
+import java.io.Serializable
+import java.util.*
+import javax.persistence.*
+
+@Entity
+class Categoria(
+        var nombre: String,
+        var urlImagen: String,
+        @ManyToOne var padre: Categoria? = null,
+        @Id @GeneratedValue val id : Long? = null
+) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || javaClass != other.javaClass) return false
+        val that = other as Categoria
+        if (id != that.id) return false
+        return true
+    }
+
+
+    override fun hashCode(): Int {
+        return if (id != null)
+            id.hashCode()
+        else 0
+    }
+
+
+}
+
+@Entity
+class Producto(
+        var nombre: String,
+        var precio: Double,
+        @Lob var descripcion: String,
+        var urlImage: String,
+        @ManyToOne var categoria: Categoria,
+        @Id @GeneratedValue val id : Long? = null
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || javaClass != other.javaClass) return false
+        val that = other as Producto
+        if (id != that.id) return false
+        return true
+    }
+
+
+    override fun hashCode(): Int {
+        return if (id != null)
+            id.hashCode()
+        else 0
+    }
+}
+
+
+@Entity
+class Usuario(
+        var username : String,
+        var password: String,
+        var email: String,
+        var fullName: String,
+        @Id @GeneratedValue val id : Long? = null
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || javaClass != other.javaClass) return false
+        val that = other as Usuario
+        if (id != that.id) return false
+        return true
+    }
+
+
+    override fun hashCode(): Int {
+        return if (id != null)
+            id.hashCode()
+        else 0
+    }
+}
+
+@Embeddable
+class ValoracionPK(
+        @Column(name="producto_id") var productoId: Long,
+        @Column(name="usuario_id") var usuarioId: Long
+) :Serializable {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || javaClass != other.javaClass) return false
+        val that = other as ValoracionPK
+        return Objects.equals(productoId, that.productoId)
+                && Objects.equals(usuarioId, that.usuarioId)
+    }
+
+    override fun hashCode(): Int {
+        return if (productoId != null && usuarioId != null)
+            Objects.hash(productoId, usuarioId)
+        else 0
+    }
+
+}
+
+@Entity
+class Valoracion(
+        @EmbeddedId var id: ValoracionPK,
+
+        @ManyToOne
+        @MapsId("productoId")
+        @JoinColumn(name="producto_id")
+        var producto: Producto,
+
+        @ManyToOne
+        @MapsId("usuarioId")
+        @JoinColumn(name="usuario_id")
+        var usuario: Usuario,
+
+        var meGusta: Boolean,
+
+        var valoracion: Int
+
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || javaClass != other.javaClass) return false
+        val that = other as Valoracion
+        if (id != that.id) return false
+        return true
+    }
+
+
+    override fun hashCode(): Int {
+        return if (id != null)
+            id.hashCode()
+        else 0
+    }
+}
